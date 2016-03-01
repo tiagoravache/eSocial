63fa05793712e5368505913c35e2976ed37cae8f
GuilhermeSilvaCosta <tec.guilherme.dsc@gmail.com>
2016-03-01 09:01:18 -0300

    Alterações para validação com o XSD
    - No metodo "gerarinfoEstagiario", na geração da tag, foi convertido para String as atributos "natEstagio" e "nivEstagio"
    - O nome do XSD para validação estava incorreto.

--- Fontes/ACBrDFe/ACBreSocial2.0/eSocial_S2305.pas
+++ Fontes/ACBrDFe/ACBreSocial2.0/eSocial_S2305.pas
@@ -5,32 +5,32 @@
 {                                                                              }
 { Direitos Autorais Reservados (c) 2008 Wemerson Souto                         }
 {                                       Daniel Simoes de Almeida               }
-{                                       AndrÈ Ferreira de Moraes               }
+{                                       Andr√© Ferreira de Moraes               }
 {                                                                              }
 { Colaboradores nesse arquivo:                                                 }
 {                                                                              }
-{  VocÍ pode obter a ˙ltima vers„o desse arquivo na pagina do Projeto ACBr     }
+{  Voc√™ pode obter a √∫ltima vers√£o desse arquivo na pagina do Projeto ACBr     }
 { Componentes localizado em http://www.sourceforge.net/projects/acbr           }
 {                                                                              }
 {                                                                              }
-{  Esta biblioteca È software livre; vocÍ pode redistribuÌ-la e/ou modific·-la }
-{ sob os termos da LicenÁa P˙blica Geral Menor do GNU conforme publicada pela  }
-{ Free Software Foundation; tanto a vers„o 2.1 da LicenÁa, ou (a seu critÈrio) }
-{ qualquer vers„o posterior.                                                   }
+{  Esta biblioteca √© software livre; voc√™ pode redistribu√≠-la e/ou modific√°-la }
+{ sob os termos da Licen√ßa P√∫blica Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a vers√£o 2.1 da Licen√ßa, ou (a seu crit√©rio) }
+{ qualquer vers√£o posterior.                                                   }
 {                                                                              }
-{  Esta biblioteca È distribuÌda na expectativa de que seja ˙til, porÈm, SEM   }
-{ NENHUMA GARANTIA; nem mesmo a garantia implÌcita de COMERCIABILIDADE OU      }
-{ ADEQUA«√O A UMA FINALIDADE ESPECÕFICA. Consulte a LicenÁa P˙blica Geral Menor}
-{ do GNU para mais detalhes. (Arquivo LICEN«A.TXT ou LICENSE.TXT)              }
+{  Esta biblioteca √© distribu√≠da na expectativa de que seja √∫til, por√©m, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia impl√≠cita de COMERCIABILIDADE OU      }
+{ ADEQUA√á√ÉO A UMA FINALIDADE ESPEC√çFICA. Consulte a Licen√ßa P√∫blica Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICEN√áA.TXT ou LICENSE.TXT)              }
 {                                                                              }
-{  VocÍ deve ter recebido uma cÛpia da LicenÁa P˙blica Geral Menor do GNU junto}
-{ com esta biblioteca; se n„o, escreva para a Free Software Foundation, Inc.,  }
-{ no endereÁo 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
-{ VocÍ tambÈm pode obter uma copia da licenÁa em:                              }
+{  Voc√™ deve ter recebido uma c√≥pia da Licen√ßa P√∫blica Geral Menor do GNU junto}
+{ com esta biblioteca; se n√£o, escreva para a Free Software Foundation, Inc.,  }
+{ no endere√ßo 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Voc√™ tamb√©m pode obter uma copia da licen√ßa em:                              }
 { http://www.opensource.org/licenses/lgpl-license.php                          }
 {                                                                              }
-{ Daniel Simıes de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
-{              PraÁa Anita Costa, 34 - TatuÌ - SP - 18270-410                  }
+{ Daniel Sim√µes de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{              Pra√ßa Anita Costa, 34 - Tatu√≠ - SP - 18270-410                  }
 {                                                                              }
 {******************************************************************************}
 
@@ -38,7 +38,9 @@
 |* Historico
 |*
 |* 27/10/2015: Jean Carlo Cantu, Tiago Ravache
-|*  - DoaÁ„o do componente para o Projeto ACBr
+|*  - Doa√ß√£o do componente para o Projeto ACBr
+|* 01/03/2016: Guilherme Costa
+|*  - Altera√ß√µes para valida√ß√£o com o XSD
 ******************************************************************************}
 {$I ACBr.inc}
 
@@ -241,8 +243,8 @@ end;
 procedure TEvtTSVAltContr.gerarinfoEstagiario(obj: TinfoEstagiario);
 begin
   Gerador.wGrupo('infoEstagiario');
-    Gerador.wCampo(tcStr, '', 'natEstagio', 0,0,0, obj.natEstagio);
-    Gerador.wCampo(tcStr, '', 'nivEstagio', 0,0,0, obj.nivEstagio);
+    Gerador.wCampo(tcStr, '', 'natEstagio', 0,0,0, eSTpNatEstagioToStr(obj.natEstagio));
+    Gerador.wCampo(tcStr, '', 'nivEstagio', 0,0,0, eStpNivelEstagioToStr(obj.nivEstagio));
     Gerador.wCampo(tcStr, '', 'areaAtuacao', 0,0,0, obj.areaAtuacao);
     Gerador.wCampo(tcStr, '', 'nrApol', 0,0,0, obj.nrApol);
     Gerador.wCampo(tcDe2, '', 'vlrBolsa', 0,0,0, obj.vlrBolsa);
@@ -287,17 +289,17 @@ end;
 function TEvtTSVAltContr.GerarXML: boolean;
 begin
   try
-    GerarCabecalho('');
-      Gerador.wGrupo('evtTSVAlteracao Id="'+GerarChaveEsocial(now, self.ideEmpregador.NrInsc, 0)+'"');//versao="'+self.versao+'"
+    GerarCabecalho('evtTSVAltContratual');
+      Gerador.wGrupo('evtTSVAltContr Id="'+GerarChaveEsocial(now, self.ideEmpregador.NrInsc, 0)+'"');//versao="'+self.versao+'"
         gerarIdeEvento2(self.IdeEvento);
         gerarIdeEmpregador(self.IdeEmpregador);
         gerarideTrabSemVinc(self.IdeTrabSemVinc);
         gerarInfoTSVAlteracao(self.infoTSVAlteracao);
-      Gerador.wGrupo('/evtTSVAlteracao');
+      Gerador.wGrupo('/evtTSVAltContr');
     GerarRodape;
 
-    XML := Assinar(Gerador.ArquivoFormatoXML, 'evtTSVAlteracao');
-    Validar('evtTSVAlteracao');
+    XML := Assinar(Gerador.ArquivoFormatoXML, 'evtTSVAltContr');
+    Validar('evtTSVAltContratual');
   except on e:exception do
     raise Exception.Create(e.Message);
   end;
